7656dade1a23f5302f2f0c6eae19b648f9f9af3d
Salami Ibrahim <hizeetech@gmail.com>
2025-03-01 13:39:09 +0100

    Business opening hours features

--- marketplace/views.py
+++ marketplace/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import get_object_or_404, redirect, render
 
 from .context_processors import get_cart_counter, get_cart_amounts
 from .models import Cart
-from vendor.models import Vendor
+from vendor.models import OpeningHour, Vendor
 from store.models import Category, ProductItem
 from django.db.models import Prefetch
 from django.contrib.auth.decorators import login_required
@@ -12,6 +12,7 @@ from django.db.models import Q
 from django.contrib.gis.geos import GEOSGeometry
 from django.contrib.gis.measure import D  # ``D`` is a shortcut for ``Distance``
 from django.contrib.gis.db.models.functions import Distance
+from datetime import date, datetime
 
 
 def marketplace(request):
@@ -23,25 +24,37 @@ def marketplace(request):
     }
     return render(request, 'marketplace/listings.html', context)
 
+from datetime import date
+from datetime import datetime
+from django.utils.timezone import localtime, now
+
 def vendor_detail(request, vendor_slug):
     vendor = get_object_or_404(Vendor, vendor_slug=vendor_slug)
-    
+
     categories = Category.objects.filter(vendor=vendor).prefetch_related(
         Prefetch(
             'productitems',
             queryset = ProductItem.objects.filter(is_available=True)
         )
     )
+
+    opening_hours = OpeningHour.objects.filter(vendor=vendor).order_by('day', 'from_hour')
     
+    # Check current day's opening hours.
+    today_date = date.today()
+    today = today_date.isoweekday()
+    
+    current_opening_hours = OpeningHour.objects.filter(vendor=vendor, day=today)    
     if request.user.is_authenticated:
         cart_items = Cart.objects.filter(user=request.user)
     else:
         cart_items = None
-    
     context = {
         'vendor': vendor,
         'categories': categories,
         'cart_items': cart_items,
+        'opening_hours': opening_hours,
+        'current_opening_hours': current_opening_hours,
     }
     return render(request, 'marketplace/vendor_detail.html', context)
 
--- vendor/forms.py
+++ vendor/forms.py
@@ -1,6 +1,6 @@
 # vendor/forms.py
 from django import forms
-from .models import Vendor
+from .models import OpeningHour, Vendor
 from accounts.validators import allow_only_images_validator
 
 
@@ -9,4 +9,10 @@ class VendorForm(forms.ModelForm):
     class Meta:
         model = Vendor
         fields = ['vendor_name', 'vendor_license']
+        
+        
+class OpeningHourForm(forms.ModelForm):
+    class Meta:
+        model = OpeningHour
+        fields = ['day', 'from_hour', 'to_hour', 'is_closed']
 
