d32b5ec136355e44087f8485c5e176c2517756bf
Salami Ibrahim <hizeetech@gmail.com>
2025-03-02 00:47:11 +0100

    Customer App +  Profile Update

--- accounts/context_processors.py
+++ accounts/context_processors.py
@@ -1,6 +1,7 @@
 # accounts/context_processors.py
 from vendor.models import Vendor
 from django.conf import settings
+from accounts.models import UserProfile
 
 def get_vendor(request):
     try:
@@ -8,6 +9,14 @@ def get_vendor(request):
     except:
         vendor = None
     return dict(vendor=vendor)
+
+def get_user_profile(request):
+    try:
+        user_profile = UserProfile.objects.get(user=request.user)
+    except:
+        user_profile = None
+    return dict(user_profile=user_profile)
+
 
 def get_google_api(request):
     return {'GOOGLE_API_KEY': settings.GOOGLE_API_KEY}
