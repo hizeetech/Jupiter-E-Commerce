031d61a66a8308f2b02c0790b1ae2d09a3e7c522
Salami Ibrahim <hizeetech@gmail.com>
2025-03-11 21:22:40 +0100

    Static file collections

--- eazy_main/settings.py
+++ eazy_main/settings.py
@@ -159,6 +159,9 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
 
+DATA_UPLOAD_MAX_MEMORY_SIZE = 104857600  # 100MB
+FILE_UPLOAD_MAX_MEMORY_SIZE = 104857600  # 100MB
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
